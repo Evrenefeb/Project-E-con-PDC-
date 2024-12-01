7a8a0c35950eb56f49e7aac646d8f46903fd863a
Evrenefeb <balcievrenefe@gmail.com>
2024-12-01 16:09:25 +0300

    Added fetching for IPv4 Address

--- PWST_v0.3_server/Program.cs
+++ PWST_v0.3_server/Program.cs
@@ -1,14 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
 
 namespace PWST_v0._3_server {
     internal class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Server\n\n\n\n");
+
+            Thread.Sleep(1000);
+            StartLocal();
+
+            Console.ReadKey();
+
+        }
+
+
+        private static void StartLocal() {
+            Console.WriteLine("Getting IPv4 Address ...");
+            Thread.Sleep(1000);
+
+            string localIPv4, localhostName;
+
+            localhostName = Dns.GetHostName();
+            IPHostEntry iPHostEntry;
+            iPHostEntry = Dns.GetHostEntry(localhostName);
+
+            // This part checks for IPv4 and gets it into string localIPv4
+
+            if (iPHostEntry.AddressList != null) {
+                Console.WriteLine("----- IPv4 -----");
+                foreach (IPAddress address in iPHostEntry.AddressList) {
+                    if (address.AddressFamily.ToString().Equals("InterNetwork")) {
+                        Thread.Sleep(100);
+                        localIPv4 = address.ToString();
+                        Console.WriteLine("Host: " + localhostName + " --> IPv4: " + localIPv4);
+                    }
+                }
+            }
+
+
+
 
         }
     }
