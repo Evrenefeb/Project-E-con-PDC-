be2813dc927846cc4d89bf425732e17515732558
Evrenefeb <evrenefebalci@gmail.com>
2024-12-02 15:27:28 +0300

    Added the basis of Recording Audio BEWEAR: WIP

--- PWST_v0.3_client/Utilities/AudioHandler.cs
+++ PWST_v0.3_client/Utilities/AudioHandler.cs
@@ -1,4 +1,5 @@
 ﻿using NAudio.Wave;
+using System;
 
 namespace PWST_v0._3_client.Utilities {
     public class AudioHandler {
@@ -12,6 +13,19 @@ namespace PWST_v0._3_client.Utilities {
 
             // Build it using example https://www.youtube.com/watch?v=6y9sAtPtTac&list=PLrqwM2iFaguiHMxomYvX01EJk82fuD8g_&index=4
             // https://github.com/naudio/NAudio/blob/master/NAudio.Core/Wave/WaveOutputs/WaveFileWriter.cs
+
+            string fileToWrite = null;
+
+            WaveFileWriter waveFileWriter = new WaveFileWriter(fileToWrite, new WaveFormat(44100,1));
+            WaveInEvent inEvent = new WaveInEvent();
+            inEvent.StartRecording();
+
+            Console.ReadKey();
+            inEvent.StopRecording();
+
+            inEvent.Dispose();
+
+
 
         }
 
