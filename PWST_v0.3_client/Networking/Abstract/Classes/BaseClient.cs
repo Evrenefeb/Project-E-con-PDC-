c86d5a61ef38f4777ece46a40995d3c5c318121f
Evrenefeb <balcievrenefe@gmail.com>
2024-12-02 21:11:56 +0300

    Seperated Client with abstractions

---
+++ PWST_v0.3_client/Networking/Abstract/Classes/BaseClient.cs
@@ -0,0 +1,23 @@
+﻿using WebSocketSharp;
+
+namespace PWST_v0._3_client.Networking.Abstract.Classes {
+    public abstract class BaseClient {
+
+        // Properties
+        public int SessionID { get; private set; }
+        public string Username { get; protected set; }
+        public WebSocket WebSocket { get; protected set; }
+
+        // Constructors
+        protected BaseClient() {
+            Username = "Anonymous";
+        }
+        protected BaseClient(string username) {
+            Username = username;
+        }
+
+        // Methods
+        public abstract void Connect();
+        public abstract void Disconnect();
+    }
+}
---
+++ PWST_v0.3_client/Networking/Concrete/ConsoleClient.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using WebSocketSharp;
+
+using PWST_v0._3_client.Networking.Abstract.Classes;
+
+
+namespace PWST_v0._3_client.Networking.Concrete {
+    public class ConsoleClient : BaseClient {
+
+        // Properties
+
+        // Constructors
+        public ConsoleClient() : base(){
+            
+        }
+        public ConsoleClient(string username) : base(username) {
+            Username = username;
+        }
+
+        // Methods
+        public override void Connect() {
+            string url = null, addressToConnect = null;
+            int port = 0;
+
+            Console.Write("Enter server IPv4 to connect : ");
+            addressToConnect = Console.ReadLine();
+
+            Console.Write("Enter server port to connect : ");
+            string answerPort = Console.ReadLine();
+
+
+            if (int.TryParse(answerPort, out port)) {
+                url = "ws://" + addressToConnect + ":" + port + "/echo-all";
+            }
+            else {
+                Console.WriteLine("Could not connected to any server.");
+                Console.ReadKey();
+            }
+
+
+            WebSocket = new WebSocket(url);
+
+            WebSocket.OnMessage += WebSocket_OnMessage;
+
+
+
+            WebSocket.Connect();
+            if (WebSocket.IsAlive) {
+                Console.WriteLine("Connection established.");
+            }
+            else {
+                Console.WriteLine("Connection could not established");
+            }
+
+
+            // TODO: Implement more mods
+            ChatMode();
+        }
+
+        private void ChatMode() {
+            Console.WriteLine("Permentant Chat Mode: ENABLED (To exit type \"dc\") \n\n");
+
+            while (true) {
+                Console.Write(Username + " > ");
+                string message = Console.ReadLine();
+                if (message.Equals("dc")) Disconnect();
+
+                WebSocket.Send("[" + DateTime.Now + "] 'Client' >>> " + message);
+            }
+        }
+
+        public override void Disconnect() {
+            WebSocket?.Close();
+        }
+
+        // Event Handlers
+        private void WebSocket_OnMessage(object sender, MessageEventArgs e) {
+            if(sender != this) {
+                //Console.WriteLine("Client recieved data from Server: " + e.Data);
+            }
+        }
+    }
+}
