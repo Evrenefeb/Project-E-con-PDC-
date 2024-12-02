3532b72361b13594871f3cc1a4bc4d024ce52519
Evrenefeb <balcievrenefe@gmail.com>
2024-12-02 22:50:14 +0300

    Failed attemp #1 for Audio restored

---
+++ PWST_v0.3_client/Utilities/AudioReciever.cs
@@ -0,0 +1,32 @@
+﻿using NAudio.Wave;
+using System;
+using System.IO;
+using WebSocketSharp;
+
+namespace PWST_v0._3_client.Utilities {
+    internal class AudioReciever {
+        private WaveOut waveOut;
+        private WebSocket webSocket;
+
+        public void StartListening(WebSocket openWebSocket) {
+            webSocket = openWebSocket;  
+            waveOut = new WaveOut();
+
+            webSocket.OnMessage += OnAudioRecieved;
+            webSocket.Connect();
+
+        }
+
+        private void OnAudioRecieved(object sender, MessageEventArgs e) {
+
+            byte[] audioBuffer = Convert.FromBase64String(e.Data);
+
+            using (var memoryStream = new MemoryStream(audioBuffer)) {
+                using (var rawSourceWaveSream = new RawSourceWaveStream(memoryStream, new WaveFormat(44100, 16, 2) ) ) {
+                    waveOut.Init(rawSourceWaveSream);
+                    waveOut.Play();
+                }
+            }
+        }
+    }
+}
---
+++ PWST_v0.3_client/Utilities/AudioTransmitter.cs
@@ -0,0 +1,29 @@
+﻿using NAudio.Wave;
+using System;
+using WebSocketSharp;
+
+namespace PWST_v0._3_client.Utilities {
+    internal class AudioTransmitter {
+        private WaveInEvent waveIn;
+        private WebSocket webSocket;
+
+        public void StartTransmisson(WebSocket openWebSocket) {
+            webSocket = openWebSocket;
+
+            waveIn = new WaveInEvent();
+            waveIn.WaveFormat = new WaveFormat(44100, 16, 2);
+            waveIn.BufferMilliseconds = 50;
+
+            waveIn.DataAvailable += OnWaveInDataAvailable;
+            waveIn.StartRecording();
+
+
+        }
+
+        private void OnWaveInDataAvailable(object sender, WaveInEventArgs e) {
+            string audioData = Convert.ToBase64String(e.Buffer);
+
+            webSocket.Send(audioData);
+        }
+    }
+}
